4bd33ab7d1b3adac2ce3473e0463f57bcdae6cab
Павел Осипенко <amtspraggy@gmail.com>
2021-01-10 22:08:29 +0200

    Add project files.

---
+++ addressbook_tests_autoit/tests/GroupCreationTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData newGroup = new GroupData()
+            {
+                Name = "Test"
+            };
+
+            app.Groups.Add(newGroup);
+
+            List <GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);            
+        }
+    }
+}
